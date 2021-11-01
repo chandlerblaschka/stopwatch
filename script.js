0546119cd35520b9778925f54f9eda0711cef709
chandlerblaschka <cblaschka@live.com>
2021-11-01 18:50:41 -0500

    two button

--- script.js
+++ script.js
@@ -10,9 +10,9 @@
     var addSecs = document.getElementById("sec");
     var addMins = document.getElementById("mins");
     var btnStart = document.getElementById("start");
-    var btnStop = document.getElementById("stop");
+    // var btnStop = document.getElementById("stop");
     var btnReset = document.getElementById("reset");
-    var btnLap = document.getElementById("lap");
+    // var btnLap = document.getElementById("lap");
     var interval; //not sure what this does
     var lapCounter = 0;
     var lapInterval;
@@ -22,105 +22,72 @@
     let maxLaps =[];
     let min = 10000000000000000000;
     let minLaps = [];
+    let clickCount = 0;
 
-    btnLap.disabled = true;
+    //btnLap.disabled = true;
 
-    btnStart.addEventListener("click", () => {
-        clearInterval(interval);
-        btnStop.style.borderColor = 'red';
-        btnStop.style.color = 'red';
-        btnStart.style.borderColor = 'black';
-        btnStart.style.color = 'black';
-        interval = setInterval(startTimer, 10);
-        lapInterval = setInterval(startLapTimer, 10);
-        btnLap.disabled = false;
-        btnStart.disabled = true;
-        btnStop.disabled = false;
-    })
-
-    btnStop.addEventListener("click", () => {
-        btnStop.style.borderColor = 'black';
-        btnStop.style.color = 'black';
-        btnStart.style.borderColor = 'green';
-        btnStart.style.color = 'green';
-        clearInterval(interval);
-        clearInterval(lapInterval);
-        btnLap.disabled = true;
-        btnStart.disabled = false;
-    })
-
-    btnLap.addEventListener("click", () => {
-        lapCounter++
-        
-        let currentLap = lapMinutes + lapSeconds + lapTens;
-        arrLap.push(currentLap)
-
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute('id', `lap${lapCounter}`);
-        //newDiv.setAttribute('id', 'clear');
-        let node = document.createTextNode(`Lap ${lapCounter} - ${lapMinutes}:${lapSeconds}:${lapTens}`);
-        newDiv.prepend(node);
-        let lapList = document.getElementById('laps');
-        lapList.prepend(newDiv);
-        clearInterval(lapInterval);
-
-
-        lapTens = "00";
-        lapSeconds = "00";
-        lapMinutes = "00";
-
-        lapInterval = setInterval(startLapTimer, 10);
-
-
-        for(let i = 0; i < arrLap.length; i++){
-            if(arrLap[i] > max && i > 0){
-                max = arrLap[i];
-                index = 'lap'+(i+1);
-                document.getElementById(`${index}`).style.color = 'red';
-                for(let i = 0; i < maxLaps.length; i++){
-                    document.getElementById(`${maxLaps[i]}`).style.color = 'black';
-                }
-                maxLaps.push(index);
-            }
-
-            if (arrLap[i] < max && arrLap[i] < min){
-                min = arrLap[i];
-                index = 'lap'+(i+1);
-                document.getElementById(`${index}`).style.color = 'green';
-                for(let i = 0; i < minLaps.length; i++){
-                    document.getElementById(`${minLaps[i]}`).style.color = 'black';
-                }
-                minLaps.push(index);
-                console.log(min)
-            }
-
-            if(arrLap[0] >= max){
-                max = arrLap[0];
-                index = 'lap'+(0+1);
-                document.getElementById(`${index}`).style.color = 'red';
-                maxLaps.push(index);
-            }
-
-            if(arrLap[0] <= min){
-                min = arrLap[0];
-                index = 'lap'+(0+1);
-                document.getElementById(`${index}`).style.color = 'green';
-                minLaps.push(index);
-                console.log(min)
-            }
-            
+    btnStart.addEventListener('click', () => {
+        clickCount++
+        console.log(clickCount)
+        if(clickCount % 2 === 0){
+            clearInterval(interval);
+            clearInterval(lapInterval)
+            btnStart.style.color = 'green'
+            btnStart.style.borderColor = 'green'
+            btnStart.textContent = 'start'
+            btnReset.textContent = 'reset'
+        } else {
+            clearInterval(interval);
+            interval = setInterval(startTimer, 10);
+            lapInterval = setInterval(startLapTimer, 10)
+            btnStart.style.color = 'red'
+            btnStart.style.borderColor = 'red'
+            btnStart.textContent = 'stop'
+            btnReset.textContent = 'lap'
         }
-
+        
     })
+
+
+    // btnStart.addEventListener("click", () => {
+    //     clearInterval(interval);
+    //     btnStop.style.borderColor = 'red';
+    //     btnStop.style.color = 'red';
+    //     btnStart.style.borderColor = 'black';
+    //     btnStart.style.color = 'black';
+    //     interval = setInterval(startTimer, 10);
+    //     lapInterval = setInterval(startLapTimer, 10);
+    //     btnLap.disabled = false;
+    //     btnStart.disabled = true;
+    //     btnStop.disabled = false;
+    // })
+
+    // btnStop.addEventListener("click", () => {
+    //     btnStop.style.borderColor = 'black';
+    //     btnStop.style.color = 'black';
+    //     btnStart.style.borderColor = 'green';
+    //     btnStart.style.color = 'green';
+    //     clearInterval(interval);
+    //     clearInterval(lapInterval);
+    //     btnLap.disabled = true;
+    //     btnStart.disabled = false;
+    // })
+
+    // btnLap.addEventListener("click", () => {
+        
+    // })
 
     btnReset.addEventListener("click", () => {
-        btnStop.style.borderColor = 'black';
-        btnStop.style.color = 'black';
+        if(clickCount % 2 === 0){
+        btnReset.textContent = 'reset'
+        //console.log(clickCount)
+        // btnStop.style.borderColor = 'black';
+        // btnStop.style.color = 'black';
         btnStart.style.borderColor = 'black';
         btnStart.style.color = 'black';
         clearInterval(interval);  
         clearInterval(lapInterval);
-        btnLap.disabled = true;  
+        //btnLap.disabled = true;  
         tens = "00";
         seconds = "00";
         minutes = "00";
@@ -141,14 +108,69 @@
         newDiv.prepend(node);
         let lapList = document.getElementById('laps');
         lapList.appendChild(newDiv);
-        btnStart.disabled = false;
-        btnStop.disabled = true;
+        // btnStart.disabled = false;
+        // btnStop.disabled = true;
         index = 0;
         max = 0;
         maxLaps = [];
         arrLap = [];
         min = 10000000000000000000;
         minLaps = [];
+        clickCount = 0;
+        } else {
+        btnReset.textContent = 'lap'
+        lapCounter++
+        let currentLap = lapMinutes + lapSeconds + lapTens;
+        arrLap.push(currentLap)
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('id', `lap${lapCounter}`);
+        //newDiv.setAttribute('id', 'clear');
+        let node = document.createTextNode(`Lap ${lapCounter} - ${lapMinutes}:${lapSeconds}:${lapTens}`);
+        newDiv.prepend(node);
+        let lapList = document.getElementById('laps');
+        lapList.prepend(newDiv);
+        clearInterval(lapInterval);
+        lapTens = "00";
+        lapSeconds = "00";
+        lapMinutes = "00";
+        lapInterval = setInterval(startLapTimer, 10);
+        for(let i = 0; i < arrLap.length; i++){
+            if(arrLap[i] > max && i > 0){
+                max = arrLap[i];
+                index = 'lap'+(i+1);
+                document.getElementById(`${index}`).style.color = 'red';
+                for(let i = 0; i < maxLaps.length; i++){
+                    document.getElementById(`${maxLaps[i]}`).style.color = 'black';
+                }
+                maxLaps.push(index);
+            }
+            if (arrLap[i] < max && arrLap[i] < min){
+                min = arrLap[i];
+                index = 'lap'+(i+1);
+                document.getElementById(`${index}`).style.color = 'green';
+                for(let i = 0; i < minLaps.length; i++){
+                    document.getElementById(`${minLaps[i]}`).style.color = 'black';
+                }
+                minLaps.push(index);
+                console.log(min)
+            }
+            if(arrLap[0] >= max){
+                max = arrLap[0];
+                index = 'lap'+(0+1);
+                document.getElementById(`${index}`).style.color = 'red';
+                maxLaps.push(index);
+            }
+            if(arrLap[0] <= min){
+                min = arrLap[0];
+                index = 'lap'+(0+1);
+                document.getElementById(`${index}`).style.color = 'green';
+                minLaps.push(index);
+                console.log(min)
+            }
+        }
+        }
+
+        
     })
 
     function startTimer() {
